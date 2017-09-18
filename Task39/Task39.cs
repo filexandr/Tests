c1ff14cf1a894bbf1b8909d8eceb0437700b2694
Oleksandr Ieremenko <oleksier@gmail.com>
2017-09-19 00:33:43 +0300

    39

---
+++ Task39/Task39.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task39
+{
+    public static class Task39
+    {
+        // 39.Given an array of size N in which every number is between 1 and N, determine if there are any
+        // duplicates in it and what kind.You are allowed to destroy or modify the array if you like.
+        // Time: maximum O(3N), 1N is for validation.
+        // Space: O(1). List is returned only for testing purposes.
+        public static List<int> GetDuplicates(int[] input)
+        {
+            var result = new List<int>();
+
+            // Validation
+            if (input == null || input.Length == 1) return result;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                var value = input[i];
+                if (value < 1 || value > input.Length)
+                {
+                    throw new ArgumentException("All values must be in range 1..N", nameof(input));
+                }
+            }
+
+            // Mark numbers in their locations because every number is between 1 and N.
+            // 1..N = original value
+            // 0    = empty
+            // -X   = X entries
+            for (var i = 0; i < input.Length; i++)
+            {
+                var value = input[i];
+
+                // Reference to itself - start counting
+                if (value == i + 1)
+                {
+                    input[i] = - 1;
+                }
+                // Reference to another cell - reset the cell and start walking
+                else if (value > 0)
+                {
+                    input[i] = 0;
+                    var newPosition = value - 1;
+                    while (newPosition >= 0)
+                    {
+                        value = input[newPosition];
+
+                        // Continue counting
+                        if (value < 1)
+                        {
+                            input[newPosition] += -1;
+                            if (input[newPosition] == -2)
+                            {
+                                result.Add(newPosition + 1);
+                            }
+
+                            newPosition = -1;
+                        }
+                        // We came to position which holds itself - duplicate!
+                        else if (value - 1 == newPosition)
+                        {
+                            input[newPosition] = -2;
+                            result.Add(newPosition + 1);
+                            newPosition = -1;
+                        }
+                        // Continue to walk
+                        else
+                        {
+                            input[newPosition] = -1;
+                            newPosition = value - 1;
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Task39/Task39UnitTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using FluentAssertions;
+
+namespace Task39
+{
+    [TestClass]
+    public class Task39UnitTest
+    {
+        [TestMethod]
+        public void Null_Positive()
+        {
+            Task39.GetDuplicates(null).Should().BeEmpty();
+        }
+
+        [TestMethod]
+        public void One_Positive()
+        {
+            Task39.GetDuplicates(new []{ 1 }).Should().BeEmpty();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ConstraintsViolation_Negative()
+        {
+            Task39.GetDuplicates(new []{ 1, -1 });
+        }
+
+        [TestMethod]
+        public void NoDuplicates_Positive()
+        {
+            Task39.GetDuplicates(new[] { 1, 2, 3, 4, 5 }).Should().BeEmpty();
+        }
+
+        [TestMethod]
+        public void OneDuplicate_Positive()
+        {
+            Task39.GetDuplicates(new[] { 1, 2, 1, 4, 5 }).Should().BeEquivalentTo(new List<int> { 1 });
+        }
+
+        [TestMethod]
+        public void SeveralDuplicates_Positive()
+        {
+            Task39.GetDuplicates(new[] { 4, 3, 2, 2, 3 }).Should().BeEquivalentTo(new List<int> { 2, 3 });
+        }
+    }
+}
