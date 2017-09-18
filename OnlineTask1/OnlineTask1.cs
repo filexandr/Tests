918fe46ba74de62a2623cf1649a167d5bfcd5bb5
Oleksandr Ieremenko <oleksier@gmail.com>
2017-09-18 11:20:45 +0300

    OnlineTask1

---
+++ OnlineTask1/OnlineTask1.cs
@@ -0,0 +1,49 @@
+﻿namespace OnlineTask1
+{
+    //Given: sorted array of integers
+    //Return: sorted array of squares of those integers.Integers can be negative.
+    //Ex: [1,3,5] -> [1,9,25]
+    public static class OnlineTask1
+    {
+        public static int[] ArrayPow2(int[] input)
+        {
+            if (input == null) return null;
+            // TODO validate on input sorting
+
+            int cur1 = 0;
+            int cur2 = input.Length - 1;
+            var output = new int[input.Length];
+            int curOut = cur2;
+            while (cur1 <= cur2)
+            {
+                int value1 = input[cur1];
+                value1 *= value1;
+
+                if (cur1 == cur2)
+                {
+                    output[curOut] = value1;
+                    break;
+                }
+
+                int value2 = input[cur2];
+                value2 *= value2;
+
+                if (value1 >= value2)
+                {
+                    output[curOut] = value1;
+                    curOut--;
+                    cur1++;
+                }
+
+                if (value1 <= value2)
+                {
+                    output[curOut] = value2;
+                    curOut--;
+                    cur2--;
+                }
+            }
+
+            return output;
+        }
+    }
+}
---
+++ OnlineTask1/OnlineTask1UnitTest.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OnlineTask1
+{
+    [TestClass]
+    public class OnlineTask1UnitTest
+    {
+        [TestMethod]
+        public void Null()
+        {
+            var actual = OnlineTask1.ArrayPow2(null);
+            Assert.IsNull(actual);
+        }
+
+        [TestMethod]
+        public void One()
+        {
+            var input = new int[] { 2 };
+            var actual = OnlineTask1.ArrayPow2(input);
+            actual.Should().Equal(new int[] { 4 });
+        }
+
+        [TestMethod]
+        public void AllNegatives()
+        {
+            var input = new int[] { -3, -2, -1 };
+            var actual = OnlineTask1.ArrayPow2(input);
+            actual.Should().Equal(new int[] { 1, 4, 9 });
+        }
+
+        [TestMethod]
+        public void AllPositivesAndEvenCount()
+        {
+            var input = new int[] { 0, 1, 3, 5 };
+            var actual = OnlineTask1.ArrayPow2(input);
+            actual.Should().Equal(new int[] { 0, 1, 9, 25 });
+        }
+
+        [TestMethod]
+        public void MixedAndOddCount()
+        {
+            var input = new int[] { -3, -2, -1, 0, 1, 3, 5 };
+            var actual = OnlineTask1.ArrayPow2(input);
+            actual.Should().Equal(new int[] { 0, 1, 1, 4, 9, 9, 25 });
+        }
+    }
+}
