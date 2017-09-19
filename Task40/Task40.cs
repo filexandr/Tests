647c33500754e6e1b8683164968a1016cf2a0107
Oleksandr Ieremenko <oleksier@gmail.com>
2017-09-19 12:16:00 +0300

    40

---
+++ Task40/Task40.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Drawing;
+
+namespace Task40
+{
+    public static class Task40
+    {
+        // 40.Write a routine to draw a circle(x** 2 + y** 2 = r** 2) without making use of any floating point
+        // computations at all.
+        // Time: O(4(R + 1) * Sqrt(R))
+        // Space: O(4(R + 1))
+        public static Point[] CalcCirclePoints(int centerX, int centerY, int radius)
+        {
+            if (radius <= 0)
+            {
+                throw new ArgumentException("Radius must be positive.", nameof(radius));
+            }
+
+            var result = new Point[(radius + 1) * 4];
+
+            var r2 = radius * radius;
+            for (var x = 0; x <= radius; x++)
+            {
+                var y = IntSqrt(r2 - x * x);
+                // Quadrant1
+                result[x] = new Point(centerX + x, centerY + y);
+                // Quadrant2
+                result[x + radius + 1] = new Point(centerX - x, centerY + y);
+                // Quadrant3
+                result[x + 2 * (radius + 1)] = new Point(centerX - x, centerY - y);
+                // Quadrant4
+                result[x + 3 * (radius + 1)] = new Point(centerX + x, centerY - y);
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Calculates the closest integer square root. Time: O(Sqrt(value))
+        /// </summary>
+        /// <param name="value">The value in power of 2.</param>
+        /// <returns>Integer square root.</returns>
+        /// <exception cref="System.ArgumentException">It's not possible to calculate square root from a negative value. - value</exception>
+        public static int IntSqrt(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("It's not possible to calculate square root from a negative value.",
+                    nameof(value));
+            }
+
+            if (value < 2) return value;
+
+            int bestDist = int.MaxValue;
+            int bestResult = 0;
+            for (var i = 1; i < value; i++)
+            {
+                var dist = Math.Abs(value - i * i);
+                if (bestDist >= dist)
+                {
+                    bestDist = dist;
+                    bestResult = i;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return bestResult;
+        }
+    }
+}
---
+++ Task40/Task40UnitTest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using FluentAssertions;
+
+namespace Task40
+{
+    [TestClass]
+    public class Task40UnitTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void IntSqrt_Negative()
+        {
+            Task40.IntSqrt(-1);
+        }
+
+        [TestMethod]
+        public void IntSqrt_Positive()
+        {
+            Task40.IntSqrt(0).Should().Equals(0);
+            Task40.IntSqrt(1).Should().Equals(1);
+            Task40.IntSqrt(2).Should().Equals(1);
+            Task40.IntSqrt(3).Should().Equals(2);
+            Task40.IntSqrt(4).Should().Equals(2);
+            Task40.IntSqrt(5).Should().Equals(2);
+            Task40.IntSqrt(6).Should().Equals(2);
+            Task40.IntSqrt(7).Should().Equals(3);
+            Task40.IntSqrt(8).Should().Equals(3);
+            Task40.IntSqrt(9).Should().Equals(3);
+            Task40.IntSqrt(10).Should().Equals(3);
+            Task40.IntSqrt(11).Should().Equals(3);
+            Task40.IntSqrt(12).Should().Equals(3);
+            Task40.IntSqrt(13).Should().Equals(4);
+            Task40.IntSqrt(14).Should().Equals(4);
+            Task40.IntSqrt(15).Should().Equals(4);
+            Task40.IntSqrt(16).Should().Equals(4);
+            Task40.IntSqrt(17).Should().Equals(4);
+            Task40.IntSqrt(18).Should().Equals(4);
+            Task40.IntSqrt(19).Should().Equals(4);
+            Task40.IntSqrt(20).Should().Equals(4);
+            Task40.IntSqrt(21).Should().Equals(5);
+            Task40.IntSqrt(22).Should().Equals(5);
+            Task40.IntSqrt(23).Should().Equals(5);
+            Task40.IntSqrt(24).Should().Equals(5);
+            Task40.IntSqrt(25).Should().Equals(5);
+            Task40.IntSqrt(26).Should().Equals(5);
+            Task40.IntSqrt(27).Should().Equals(5);
+            Task40.IntSqrt(28).Should().Equals(5);
+            Task40.IntSqrt(29).Should().Equals(5);
+            Task40.IntSqrt(30).Should().Equals(5);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Circle_Negative()
+        {
+            Task40.CalcCirclePoints(10, 10, -5);
+        }
+
+        [TestMethod]
+        public void Circle_Positive()
+        {
+            var points = Task40.CalcCirclePoints(10, 10, 5);
+            points.Should().NotBeNull();
+            points.Length.Should().Equals(6 * 4);
+            points.All(point => point.X >= -15 && point.X <= 15 && point.Y >= -15 && point.Y <= 15).Should().BeTrue();
+            points.All(point => Math.Sqrt(Math.Pow(Math.Abs(point.X - 10), 2) + Math.Pow(Math.Abs(point.Y - 10), 2)) <= 6).Should().BeTrue();
+            points.All(point => Math.Sqrt(Math.Pow(Math.Abs(point.X - 10), 2) + Math.Pow(Math.Abs(point.Y - 10), 2)) >= 4).Should().BeTrue();
+        }
+    }
+}
