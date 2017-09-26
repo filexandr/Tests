099012b0ec60b146b6d571eb898100f627cbf6ca
Oleksandr Ieremenko <oleksier@gmail.com>
2017-09-26 22:07:44 +0300

    53

---
+++ Task53/Task53.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Task53
+{
+    // 53. Write a routine that prints out a 2-D array in spiral order
+    // Time: O(N)
+    // Space: O(1)
+    public static class Task53
+    {
+        public static void PrintArrayInSpiralOrder(int[,] inputArray)
+        {
+            if (inputArray == null) return;
+
+            int yLen = inputArray.GetUpperBound(0) + 1;
+            int xLen = inputArray.GetUpperBound(1) + 1;
+
+            if (xLen == 0 && yLen == 0) return;
+
+            int x = 0;
+            int y = 0;
+            int xInc = 1;
+            int yInc = 0;
+            int processed = 0;
+            int resultLen = xLen * yLen;
+            int cycle = 0;
+
+            while (processed < resultLen)
+            {
+                var value = inputArray[y, x];
+                Console.Write($" {value}");
+                processed++;
+
+                if (xInc == 1)
+                {
+                    x++;
+                    if (x == xLen - 1 - cycle)
+                    {
+                        xInc = 0;
+                        yInc = 1;
+                    }
+                }
+                else if (xInc == -1)
+                {
+                    x--;
+                    if (x == cycle)
+                    {
+                        xInc = 0;
+                        yInc = -1;
+                    }
+                }
+                else if (yInc == 1)
+                {
+                    y++;
+                    if (y == yLen - 1 - cycle)
+                    {
+                        yInc = 0;
+                        xInc = -1;
+                    }
+                }
+                else if (yInc == -1)
+                {
+                    y--;
+                    if (y == cycle + 1)
+                    {
+                        yInc = 0;
+                        xInc = 1;
+                        cycle++;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Task53/Task53UnitTest.cs
@@ -0,0 +1,160 @@
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Smocks;
+using Smocks.Matching;
+
+namespace Task53
+{
+    [TestClass]
+    public class Task53UnitTest
+    {
+        private void ExecuteAndCheckResult(int[,] input)
+        {
+            int currentValue = 0;
+            Smock.Run(context =>
+            {
+                context.Setup(() => Console.Write(It.IsAny<string>()))
+                    .Callback<string>(text =>
+                    {
+                        var value = int.Parse(text.Trim());
+                        if (value != currentValue + 1)
+                            throw new Exception($"Printed not in right order, value: {value}, prev: {currentValue}");
+                        currentValue = value;
+                    });
+
+                Task53.PrintArrayInSpiralOrder(input);
+            });
+        }
+
+        [TestMethod]
+        public void NullAndEmpty()
+        {
+            Task53.PrintArrayInSpiralOrder(null);
+            Task53.PrintArrayInSpiralOrder(new int[,] {});
+        }
+
+        [TestMethod]
+        public void Array1X1()
+        {
+            var input = new int[,]
+            {
+                {1}
+            };
+
+            ExecuteAndCheckResult(input);
+        }
+
+        [TestMethod]
+        public void Array2X1()
+        {
+            var input = new int[,]
+            {
+                {1, 2}
+            };
+
+            ExecuteAndCheckResult(input);
+        }
+
+        [TestMethod]
+        public void Array3X1()
+        {
+            var input = new int[,]
+            {
+                {1, 2, 3}
+            };
+
+            ExecuteAndCheckResult(input);
+        }
+
+        [TestMethod]
+        public void Array2X2()
+        {
+            var input = new int[,]
+            {
+                {1, 2},
+                {4, 3}
+            };
+
+            ExecuteAndCheckResult(input);
+        }
+
+        [TestMethod]
+        public void Array3X2()
+        {
+            var input = new int[,]
+            {
+                {1, 2, 3},
+                {6, 5, 4}
+            };
+
+            ExecuteAndCheckResult(input);
+        }
+
+        [TestMethod]
+        public void Array2X3()
+        {
+            var input = new int[,]
+            {
+                {1, 2},
+                {6, 3},
+                {5, 4}
+            };
+
+            ExecuteAndCheckResult(input);
+        }
+
+        [TestMethod]
+        public void Array4X2()
+        {
+            var input = new int[,]
+            {
+                {1, 2, 3, 4},
+                {8, 7, 6, 5}
+            };
+
+            ExecuteAndCheckResult(input);
+        }
+
+        [TestMethod]
+        public void Array3X3()
+        {
+            var input = new int[,]
+            {
+                {1, 2, 3},
+                {8, 9, 4},
+                {7, 6, 5}
+            };
+
+            ExecuteAndCheckResult(input);
+        }
+
+        [TestMethod]
+        public void Array4X3()
+        {
+            var input = new int[,]
+            {
+                {1,  2,  3,  4},
+                {10, 11, 12, 5},
+                {9,  8,  7,  6}
+            };
+
+            ExecuteAndCheckResult(input);
+        }
+
+        [TestMethod]
+        public void Array4X4()
+        {
+            var input = new int[,]
+            {
+                {1,  2,  3,  4},
+                {12, 13, 14, 5},
+                {11, 16, 15, 6},
+                {10, 9,  8,  7}
+            };
+
+            ExecuteAndCheckResult(input);
+        }
+    }
+}
