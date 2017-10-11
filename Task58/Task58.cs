3eaa2a6c64be6f7f6c3a8f4339ba64461e3a7d00
Oleksandr Ieremenko <oleksier@gmail.com>
2017-10-11 21:38:14 +0300

    58

---
+++ Task58/Task58.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+
+namespace Task58
+{
+    // 58.Given an array of length N containing integers between 1 and N, determine if it contains any
+    // duplicates
+    // Time: O(n)
+    // Space: O(n)
+    public static class Task58
+    {
+        public static bool ContainDuplicates(int[] input)
+        {
+            if (input == null || input.Length < 2) return false;
+
+            var vector = new BitArray(input.Length);
+            foreach (var item in input)
+            {
+                if (item < 1 || item > input.Length)
+                {
+                    throw new ArgumentException($"All values must be in range 1..{input.Length} but found {item}.");
+                }
+
+                if (vector.Get(item - 1)) return true;
+                vector.Set(item - 1, true);
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Task58/Task58UnitTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using FluentAssertions;
+
+namespace Task58
+{
+    [TestClass]
+    public class Task58UnitTest
+    {
+        [TestMethod]
+        public void EmptyAndSingle()
+        {
+            Task58.ContainDuplicates(null).Should().Be(false);
+            Task58.ContainDuplicates(new int[0]).Should().Be(false);
+            Task58.ContainDuplicates(new int[] {1}).Should().Be(false);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void WrongEntry()
+        {
+            Task58.ContainDuplicates(new int[] {10, 10});
+        }
+
+        [TestMethod]
+        public void NoDuplicates()
+        {
+            Task58.ContainDuplicates(new int[] {1, 2, 3, 4, 5}).Should().Be(false);
+        }
+
+        [TestMethod]
+        public void Duplicates()
+        {
+            Task58.ContainDuplicates(new int[] {1, 2, 3, 4, 5, 5}).Should().Be(true);
+            Task58.ContainDuplicates(new int[] {1, 1, 2, 3, 4, 5, 2}).Should().Be(true);
+        }
+    }
+}
