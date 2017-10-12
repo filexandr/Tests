ca39d0981efc013df0367666e3a65000adb90216
Oleksandr Ieremenko <oleksier@gmail.com>
2017-10-12 09:48:05 +0300

    59

---
+++ Task59/Task59.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task59
+{
+    // 59.An array of integers.The sum of the array is known not to overflow an integer. Compute the sum.
+    // What if we know that integers are in 2's complement form?    // Time: O(32*n)    // Space: O(1)    public static class Task59
+    {
+        public static int CalcSum(int[] input)
+        {
+            if (input == null || input.Length == 0) throw new ArgumentException("Input is empty.");
+
+            int result = 0;
+            int bitValue = 1;
+            var bitCount = 0;
+            for (int bit = 0; bit < 32; bit++)
+            {
+                for (int i = 0; i < input.Length; i++)
+                {
+                    bitCount += (input[i] & bitValue) == bitValue ? 1 : 0;
+                }
+
+                if (bitCount % 2 != 0) result |= bitValue;
+                bitCount /= 2;
+                bitValue *= 2;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Task59/Task59UnitTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using FluentAssertions;
+
+namespace Task59
+{
+    [TestClass]
+    public class Task59UnitTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Null()
+        {
+            Task59.CalcSum(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Empty()
+        {
+            Task59.CalcSum(new int[0]);
+        }
+
+        [TestMethod]
+        public void Test()
+        {
+            Task59.CalcSum(new[] { 1 }).Should().Be(1);
+            Task59.CalcSum(new[] { -1 }).Should().Be(-1);
+            Task59.CalcSum(new[] { -1, 1 }).Should().Be(0);
+            Task59.CalcSum(new[] {1, 2, 3}).Should().Be(6);
+            Task59.CalcSum(new[] {10, 100, -50, 40, -25}).Should().Be(75);
+            Task59.CalcSum(new[] {-10, -100, 50, -40, 25}).Should().Be(-75);
+            Task59.CalcSum(new[] {int.MaxValue, -1}).Should().Be(int.MaxValue - 1);
+            Task59.CalcSum(new[] {int.MinValue, 1}).Should().Be(int.MinValue + 1);
+        }
+    }
+}
