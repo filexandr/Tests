3d18c2b7e3c343370c4f873bbf75dc9750e3d869
Oleksandr Ieremenko <oleksier@gmail.com>
2017-10-13 13:10:50 +0300

    60

---
+++ Task60/Task60.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task60
+{
+    // 60. An array of integers of size n.Generate a random permutation of the array, given a function
+    // rand_n() that returns an integer between 1 and n, both inclusive, with equal probability.What is the
+    // expected time of your algorithm?    // Time: O(2n)    // Space: O(n)    public static class Task60
+    {
+        private static readonly Random _random = new Random();
+
+        private static int rand_n(int n)
+        {
+            return _random.Next(1, n + 1);
+        }
+
+        public static void RandomPermutation(int[] input)
+        {
+            if (input == null || input.Length < 2) return;
+
+            var set = new HashSet<int>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                set.Add(i);
+            }
+
+            while (set.Count > 0)
+            {
+                var leftIndex = rand_n(input.Length) - 1;
+                if (!set.Contains(leftIndex)) leftIndex = set.First();
+                set.Remove(leftIndex);
+
+                var rightIndex = rand_n(input.Length) - 1;
+                if (set.Count > 0)
+                {
+                    if (!set.Contains(rightIndex)) rightIndex = set.First();
+                    set.Remove(rightIndex);
+                }
+                else if (rightIndex == leftIndex)
+                {
+                    rightIndex += rightIndex < input.Length - 1 ? 1 : -1;
+                }
+
+                var leftValue = input[leftIndex];
+                input[leftIndex] = input[rightIndex];
+                input[rightIndex] = leftValue;
+            }
+        }
+    }
+}
---
+++ Task60/Task60UnitTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Task60
+{
+    [TestClass]
+    public class Task60UnitTest
+    {
+        [TestMethod]
+        public void NullAndEmpty()
+        {
+            Task60.RandomPermutation(null);
+            Task60.RandomPermutation(new int[0]);
+        }
+
+        [TestMethod]
+        public void One()
+        {
+            var input = new int[] { 1 };
+            var expected = (int[])input.Clone();
+            Task60.RandomPermutation(input);
+            input.Should().Equal(expected);
+        }
+
+        [TestMethod]
+        public void Two()
+        {
+            var input = new int[] {1, 2};
+            var notExpected = (int[])input.Clone();
+            Task60.RandomPermutation(input);
+            input.Should().NotEqual(notExpected);
+        }
+
+        [TestMethod]
+        public void Three()
+        {
+            var input = new int[] {1, 2, 3};
+            var notExpected = (int[])input.Clone();
+            Task60.RandomPermutation(input);
+            input.Should().NotEqual(notExpected);
+        }
+
+        [TestMethod]
+        public void Many()
+        {
+            var input = new int[] {10, 5, 1, 100, -7, 28, 342, 0, 98, 23, 81, 134};
+            var notExpected = (int[])input.Clone();
+            Task60.RandomPermutation(input);
+            input.Should().NotEqual(notExpected);
+        }
+    }
+}
